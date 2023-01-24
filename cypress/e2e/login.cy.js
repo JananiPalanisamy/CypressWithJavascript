5f43d5932ac9946f6088b3f23da1cd3c6b31ca67
JananiPalanisamy <jananip.cs@gmail.com>
2023-01-24 19:47:39 +0530

    grouping and running from command line

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,12 +1,52 @@
 import {loginPage} from '../pages/loginPage.cy.js'
 
 const login = new loginPage()
+describe ('all login tests', ()=>{
 
-it('Login Test', function(){
+   it('Login Test', function(){
    
-   login.navigate('https://trytestingthis.netlify.app/')
-   login.enterCredentials('test','test')
-   login.clickLoginButton()
-    
+      // login.navigate('https://trytestingthis.netlify.app/')
+      login.enterCredentials('test','test')
+      login.clickLoginButton()
+      cy.contains('Login Successful :)') 
+      cy.contains('here').click()   
+   })
+   
+   beforeEach(()=>
+   {
+      login.navigate('https://trytestingthis.netlify.app/')
+      cy.log('Running beforeEach from inside describe block')
+   })
+   
+   it.skip('Login with invalid username', function(){
+      
+     
+      login.enterCredentials('test123','test')
+      login.clickLoginButton()
+      cy.on('window:alert', (txt)=>{
+         expect(txt).to.be.equal('Wrong Credentials! Try again!')
+      })
+      // cy.contains('Login Successful :)') 
+      // cy.contains('here').click()   
+   })
+   
+   it('Login with invalid password', function(){
+      
+      // login.navigate('https://trytestingthis.netlify.app/')
+      login.enterCredentials('test','test123')
+      login.clickLoginButton()
+      cy.on('window:alert', (txt)=>{
+         expect(txt).to.be.equal('Wrong Credentials! Try again!')
+   
+      })
+      // cy.contains('Login Successful :)') 
+      // cy.contains('here').click()   
+   })
+   
+})
+
+beforeEach(()=>
+{
+   cy.log('Running beforeEachfrom outside describe block')
 })
 
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,7 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+beforeEach(()=>{
+    cy.log('running beforeEach from support file')
+})
