2938d83e52c2966eaf8e29ed18fa00933343b5e3
JananiPalanisamy <jananip.cs@gmail.com>
2023-08-05 12:07:13 +0530

    [Janani] Add Report test and update POM test

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,19 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env:{
+      baseURL:"https://naveenautomationlabs.com/opencart/"
+    },
     watchForFileChanges: true,
-    defaultCommandTimeout: 5000
+    defaultCommandTimeout: 5000,
+    reporter: "mochawesome",
+    reporterOptions: {
+      charts: true,
+      overwrite: false,
+      html: false,
+      json: true,
+      reportDir: "cypress/reports"
+    }
   },
 });
 
